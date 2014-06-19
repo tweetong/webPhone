d8ee452436544def1f02a1ccdc5e037d20848a66
MasaruIto <ito3302_110@yahoo.co.jp>
2014-06-19 14:57:56 +0900

    dsp_setのperrorを統一した

--- util/dsp_set.c
+++ util/dsp_set.c
@@ -3,22 +3,20 @@
 #include <stdio.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
-	
+
+#include "die.h"
+
 int dsp_parameters_set(int fd){
     int rate = 48000;
     int bits = 16;
 
     /* サンプリングレート値を変更 */
-    if ( ioctl( fd, SOUND_PCM_WRITE_RATE, &rate ) == -1 ) {
-        perror( "ioctl( SOUND_PCM_READ_RATE )" );
-        return -1;
-    }
+    if(ioctl( fd, SOUND_PCM_WRITE_RATE, &rate ) < 0)
+        die("ioctl( SOUND_PCM_READ_RATE )");
 	
     /* 量子化ビット数を変更 */
-    if ( ioctl( fd, SOUND_PCM_WRITE_BITS, &bits ) == -1 ) {
-        perror( "ioctl( SOUND_PCM_READ_BITS )" );
-        return -1;
-    }
+    if(ioctl( fd, SOUND_PCM_WRITE_BITS, &bits ) < 0)
+        die("ioctl( SOUND_PCM_READ_BITS )");
 	
     return 0;
 }
